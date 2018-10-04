9689cb92b55c6054c3660125a7495e80251b4e90
Jonathan Chao <jonathan.chao7@gmail.com>
2018-10-03 20:41:20 -0700

    Update primenum.java

--- primenum.java
+++ primenum.java
@@ -4,27 +4,30 @@ public class PrimeNum {
    public static void main(String args[]) {		
 	
     int temp;
+    int count =2;
     boolean isPrime = true;
     Scanner scan= new Scanner(System.in);
     System.out.println("Enter a number between 2 and 1,000: ");
     int num = scan.nextInt();
-    if (num <2)
-	
-    int i=2;
-    while(i<= num/2) {
-        if(num % i == 0) {
-            isPrime = false;
-        break;
-        }
-    i++;
+    if (num <2 || num > 1000) {
+        System.out.println("Sorry, please enter a valid number between 2 and 1,000");
     }
-	    if (isPrime) {
-	        System.out.println(num + " is a Prime Number");
-	    }
-	    else {
-	        System.out.println(num + " is not a Prime Number");
-   		}
-   scan.close();
+	else {
+        while(count <= num/2) {
+            if(num % count == 0) {
+                isPrime = false;
+            break;
+            }
+        count++;
+        }
+	        if (isPrime) {
+	            System.out.println(num + " is a Prime Number");
+	        }
+	        else {
+	            System.out.println(num + " is not a Prime Number");
+   		    }
+    scan.close();
+        }
    }
 }
 /***Jonathan Chao***/
