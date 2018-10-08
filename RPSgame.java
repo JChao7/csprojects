1c1f4361f4b1ab25e34738f4b8f0855e4d8fc252
Jonathan Chao <jonathan.chao7@gmail.com>
2018-10-07 21:04:03 -0700

    RPS Game 2.0

--- RPSgame.java
+++ RPSgame.java
@@ -1,4 +1,5 @@
-/* Rock, Paper, Scissor game in Java*/
+/* Rock, Paper, Scissor game in Java
+V 2.0 - Updated to continuously play until the computer wins more than 2 games!*/
 
 //import scanner for user input and random to generate random value
 import java.util.Scanner;
@@ -7,47 +8,54 @@ import java.util.Random;
 public class Rpsgame {
 
     public static void main(String[] args) {
-    // Prompt uswer to enter value
+    int compScore = 0;
+    while(compScore < 3) {
+    
+    //Prompt user to enter value
     Scanner scan = new Scanner(System.in);
     Random rdm = new Random();
     System.out.println("Select rock(1), paper(2), scissor(3): ");
 	int userChoice = scan.nextInt();
     int compChoice = rdm.nextInt(3) + 1;
-
-    //if/else statements
-    //user input for rock
-    if ((userChoice == 1) && (compChoice == 1)) {
-        System.out.println("You chose Rock and the computer chose Rock: Tie! Try again.");
-    }
-    if ((userChoice == 1) && (compChoice == 2)) {
-        System.out.println("You chose Rock and the computer chose Paper: Paper covers rock, You lose");
-    }
-    if ((userChoice == 1) && (compChoice == 3)) {
-        System.out.println("You chose Rock and the computer chose Scissor: Rock breaks Scissor, You win");
-    }
-    
-    //user input for Paper
-    if ((userChoice == 2) && (compChoice == 1)) {
-        System.out.println("You chose Paper and the computer chose Rock: Paper covers rock, You win");
-        }   
-    if ((userChoice == 2) && (compChoice == 2)) {
-        System.out.println("You chose Paper and the computer chose Paper: Tie! Try again.");
-    }
-    if ((userChoice == 2) && (compChoice == 3)) {
-        System.out.println("You chose Paper and the computer chose Scissor: Scissor cut Paper, You lose");
-    }
-    
-    //user input for Scissor
-    if ((userChoice == 3) && (compChoice == 1)) {
-        System.out.println("You chose Scissor and the computer chose Rock: Rock breaks Scissor, You lose");
-        }   
-    if ((userChoice == 3) && (compChoice == 2)) {
-        System.out.println("You chose Scissor and the computer chose Paper: Scissor cut Paper, You win");
-    }
-    if ((userChoice == 3) && (compChoice == 3)) {
-        System.out.println("You chose Scissor and the computer chose Scissor: Tie! Try again.");
-    }
     scan.close();
+    
+
+        //user input for rock
+        if ((userChoice == 1) && (compChoice == 1)) {
+            System.out.println("You chose Rock and the computer chose Rock: Tie! Try again.");
+        }
+        if ((userChoice == 1) && (compChoice == 2)) {
+            System.out.println("You chose Rock and the computer chose Paper: Paper covers rock, You lose");
+            compScore++;
+        }
+        if ((userChoice == 1) && (compChoice == 3)) {
+            System.out.println("You chose Rock and the computer chose Scissor: Rock breaks Scissor, You win");
+        }
+        
+        //user input for Paper
+        if ((userChoice == 2) && (compChoice == 1)) {
+            System.out.println("You chose Paper and the computer chose Rock: Paper covers rock, You win");
+            }   
+        if ((userChoice == 2) && (compChoice == 2)) {
+            System.out.println("You chose Paper and the computer chose Paper: Tie! Try again.");
+            compScore++;
+        }
+        if ((userChoice == 2) && (compChoice == 3)) {
+            System.out.println("You chose Paper and the computer chose Scissor: Scissor cut Paper, You lose");
+        }
+        
+        //user input for Scissor
+        if ((userChoice == 3) && (compChoice == 1)) {
+            System.out.println("You chose Scissor and the computer chose Rock: Rock breaks Scissor, You lose");
+            compScore++;
+            }   
+        if ((userChoice == 3) && (compChoice == 2)) {
+            System.out.println("You chose Scissor and the computer chose Paper: Scissor cut Paper, You win");
+        }
+        if ((userChoice == 3) && (compChoice == 3)) {
+            System.out.println("You chose Scissor and the computer chose Scissor: Tie! Try again.");
+            }
+        }
     }
 }
 /*** By Jonathan Chao ***/
