89d23be9c478c77c5bda300d18530d53b6c20976
Jonathan Chao <jonathan.chao7@gmail.com>
2019-03-14 09:20:35 -0700

    Update Checkers.java

--- Checkers.java
+++ Checkers.java
@@ -24,61 +24,58 @@ public class Checkers {
 
 	private static void menu() {
 		Scanner scan = new Scanner(System.in);
-		System.out.println("1. Developer");
-		System.out.println("2. Instructions on how to play");
-		System.out.println("3. Play");
-		System.out.println("4. Show Winners");
-		System.out.println("5. Exit");
-		int choice = scan.nextInt();
-		if ((choice != 3) || (choice != 5)) {
-			switch (choice) {
-			case 1:
-				System.out.println("Created by: Jonathan Chao");
-				winner();
-				break;
-			case 2:
-				System.out.println("Try and capture all your enemy pieces by jumping over your opponent pieces");
-				System.out.println(
-						"Each player can only move one piece forward diagonally at a time. \n You must jump and capture your opponents piece if there is available");
-				System.out.print(
-						"If you reach the far side of the board, you can promote your piece to king(Upper case letter) and allows it to move in any direction");
-				break;
-			case 4:
-				pastwinners();
+		int choice;
+
+		do {
+			System.out.println("1. Developer");
+			System.out.println("2. Instructions on how to play");
+			System.out.println("3. Play");
+			System.out.println("4. Show Winners");
+			System.out.println("5. Exit");
+			choice = scan.nextInt();
+			if ((choice != 3) || (choice != 5)) {
+				switch (choice) {
+				case 1:
+					System.out.println("Created by: Jonathan Chao & Edward Balase");
+					break;
+				case 2:
+					System.out.println("Try and capture all your enemy pieces by jumping over your opponent pieces");
+					System.out.println(
+							"Each player can only move one piece forward diagonally at a time. \n You must jump and capture your opponents piece if there is available");
+					System.out.print(
+							"If you reach the far side of the board, you can promote your piece to king(Upper case letter) and allows it to move in any direction");
+					break;
+				case 4:
+					pastwinners();
+				}
 			}
-		}
-		if (choice == 3) {
-			blackTurn();
-		} else
+
+		} while (choice != 5 && choice != 3);
+		if (choice == 3)
+			display();
+		else
 			System.exit(0);
 	}
 
 	public static void initializeBoard() {
-		board[0][0] = "r"; // first row
-		board[0][2] = "r";
-		board[0][4] = "r";
-		board[0][6] = "r";
-		board[1][1] = "r"; // second row
-		board[1][3] = "r";
-		board[1][5] = "r";
-		board[1][7] = "r";
-		board[2][0] = "r"; // third row
-		board[2][2] = "r";
-		board[2][4] = "r";
-		board[2][6] = "r";
 
-		board[7][1] = "b"; // first row
-		board[7][3] = "b";
-		board[7][5] = "b";
-		board[7][7] = "b";
-		board[6][0] = "b"; // second row
-		board[6][2] = "b";
-		board[6][4] = "b";
-		board[6][6] = "b";
-		board[5][1] = "b"; // third row
-		board[5][3] = "b";
-		board[5][5] = "b";
-		board[5][7] = "b";
+		int len = board.length;
+		for (int row = 0; row < len; row++) {
+			for (int col = 0; col < len; col++) {
+				board[row][col] = "-";
+			}
+		}
+
+		for (int col = 0; col < len; col = col + 2) {
+			board[0][col] = "r";
+			board[1][col + 1] = "r";
+			board[2][col] = "r";
+			board[5][col + 1] = "b";
+			board[6][col] = "b";
+			board[7][col + 1] = "b";
+
+		}
+
 	}
 
 	private static void display() {
@@ -91,12 +88,10 @@ public class Checkers {
 
 	}
 
-	public static void turn() {
-		while (1 == 1) { // this must be changed, but for now, it's in an infinite loop
-			blackTurn();
-			redTurn();
-		}
-	}
+	/*
+	 * public static void turn() { while (1 == 1) { // this must be changed, but for
+	 * now, it's in an infinite loop blackTurn(); redTurn(); } }
+	 */
 
 	private static void blackTurn() {
 		display();
@@ -108,8 +103,8 @@ public class Checkers {
 	private static void redTurn() {
 		display();
 		selectPiece();
-		redCheckLocation(selectPiece, board);
-		if (redCheckLocation(selectPiece, board) == false)
+		redCheckLocation(selectPiece(), board);
+		if (redCheckLocation(selectPiece(), board) == false)
 			selectPiece();
 		redMovement();
 	}
@@ -135,8 +130,8 @@ public class Checkers {
 	}
 
 	private static boolean blackCheckLocation() {
-		int column = selectPiece;
-		int row = selectPiece;
+		int column = selectPiece();
+		int row = selectPiece();
 		if (board[column][row] == " ")
 			return true;
 		else
@@ -173,7 +168,7 @@ public class Checkers {
 
 			FileReader fr = null;
 			try {
-				fr = new FileReader(file, true);
+				fr = new FileReader(file);
 				BufferedReader br = new BufferedReader(fr);
 
 				String data;
@@ -194,34 +189,28 @@ public class Checkers {
 			}
 
 		}
-
 	}
-
-	private static void winner() {
-		// if player has no more valid moves, the OTHER player wins
-		// displays and records the winner
-		String filename1 = "pastwinners.txt";
-		File f1 = new File(filename1);
-
-		try {
-			Date objDate = new Date();
-			System.out.println("The default date format: " + objDate.toString());
-
-			String result = formatTheDate(objDate, "MMMM d, y");
-			System.out.println("using MMMM d, y format: " + result);
-
-			String msg = String.format("%10s | %-10s", result);
-
-			FileWriter fw = new FileWriter(f1, true);
-			fw.write(msg + "\n");
-			fw.close();
-
-		} catch (IOException e) {
-
-			System.out.println(e.getMessage());
-		}
-
-	}
+	/*
+	 * private static void winner() { // if player has no more valid moves, the
+	 * OTHER player wins // displays and records the winner String filename1 =
+	 * "pastwinners.txt"; File f1 = new File(filename1);
+	 * 
+	 * try { Date objDate = new Date();
+	 * System.out.println("The default date format: " + objDate.toString());
+	 * 
+	 * String result = formatTheDate(objDate, "MMMM d, y");
+	 * System.out.println("using MMMM d, y format: " + result);
+	 * 
+	 * String msg = String.format("%10s | %-10s", result);
+	 * 
+	 * FileWriter fw = new FileWriter(f1, true); fw.write(msg + "\n"); fw.close();
+	 * 
+	 * } catch (IOException e) {
+	 * 
+	 * System.out.println(e.getMessage()); }
+	 * 
+	 * }
+	 */
 
 	private static String formatTheDate(Date theDate, String strDateFormat) {
 		String result;
